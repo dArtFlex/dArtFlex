27f171111277bdf68629dadb2dc43dfd1ef87632
Dmitriy <voytenko.d@ideasoft.io>
2021-04-19 12:22:13 +0300

    [DAR-53] created assets in store

--- src/stores/reducers/index.ts
+++ src/stores/reducers/index.ts
@@ -1,12 +1,16 @@
 import { reducer as userReducer } from './user'
+import { reducer as assetsReducer } from './assets'
 import { UserStateType } from './user/types'
+import { AssetsStateType } from './assets/types'
 
 const rootReducer = {
   user: userReducer,
+  assets: assetsReducer,
 }
 
 export interface stateType {
   user: UserStateType
+  assets: AssetsStateType
 }
 
 export default rootReducer
--- src/stores/sagas/index.ts
+++ src/stores/sagas/index.ts
@@ -1,11 +1,14 @@
 import { takeLatest, all } from 'redux-saga/effects'
 import apiMiddleware from '../../services/api_middleware'
 import { getUserDataRequest } from '../reducers/user'
+import { getAssetsRequest } from '../reducers/assets'
 import { getUserData } from '../sagas/user'
+import { getAssetsData } from '../sagas/assets'
 
 export default function* root() {
   yield all([
     /** User **/
     takeLatest(getUserDataRequest.type, getUserData, apiMiddleware),
+    takeLatest(getAssetsRequest.type, getAssetsData, apiMiddleware),
   ])
 }
