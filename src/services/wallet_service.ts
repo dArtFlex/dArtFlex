e67022b5b6510d3c21ca8d1ea18d403105332b73
Dmitriy <voytenko.d@ideasoft.io>
2021-05-19 18:47:42 +0300

    [DAR-112] fixed - button place a bit is inactive if user is not logged, MetaMask connection

--- src/services/wallet_service.ts
+++ src/services/wallet_service.ts
@@ -1,30 +1,22 @@
 //@ts-nocheck
 import BigNumber from 'bignumber.js'
+import { web3Service } from 'services/web3_service'
 
-export class WalletService {
-  constructor() {
-    if (window.web3?.currentProvider) {
-      this.web3 = window.web3
-      this.provider = window.web3.currentProvider
-    }
-  }
-
-  getMetaMaskAccount(): Promise<any> {
-    return this.provider.request({ method: 'eth_requestAccounts' })
-  }
-
-  getEthBalance(): Promise<any> {
-    return new Promise((resolve) => {
-      this.web3.eth.getCoinbase((err, account) => {
-        this.web3.eth.getBalance(account, (err, balance) => {
-          resolve(BigNumber(balance).dividedBy(10e17).toNumber())
-        })
+class WalletService {
+  async getMetaMaskAccount() {
+    const web3 = await web3Service.setWeb3EthProvider()
+    this.accounts = await web3.currentProvider.request({ method: 'eth_requestAccounts' })
+    this.balance = await new Promise((resolve) => {
+      web3.eth.getBalance(this.accounts[0], (err, balance) => {
+        resolve(BigNumber(balance).dividedBy(10e17).toNumber())
       })
     })
+    this.chainId = await ethereum.request({ method: 'eth_chainId' })
+    return this
   }
 
   getChainId(): any {
-    return ethereum.request({ method: 'eth_chainId' })
+    return this.chainId
   }
 }
 
