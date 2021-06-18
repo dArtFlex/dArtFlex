7478164870a9a41a5ea1674726fbc1b94b0aca6c
copslikedonuts <reggisen@gmail.com>
2021-06-18 12:26:57 +0300

    DAR-220-4 moved MakeOffer to another branch

--- src/pages/MakeOffer/components/Form/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as ApprovedOfferForm } from './ApprovedOfferForm'
-export { default as ApprovedOfferSubForm } from './ApprovedOfferSubForm'
-export { default as DetailsOfferForm } from './DetailsOfferForm'
-export { default as FormOfferContainer } from './FormOfferContainer'
--- src/pages/MakeOffer/components/index.ts
+++
@@ -1,5 +0,0 @@
-import About from './About'
-import History from './History'
-import { ApprovedOfferForm, ApprovedOfferSubForm, DetailsOfferForm, FormOfferContainer } from './Form'
-
-export { About, History, DetailsOfferForm, ApprovedOfferForm, ApprovedOfferSubForm, FormOfferContainer }
