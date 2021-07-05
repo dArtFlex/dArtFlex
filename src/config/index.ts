e782ea5e049091b4cf5b08b0d6e23a3e4afcecb1
Dmitriy <voytenko.d@ideasoft.io>
2021-07-05 16:23:15 +0300

    [DAR-223]  updated - stage root endpoint

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
-const BASE_URL = 'http://3.11.202.153:8888/'
-// const LOCAL_URL = 'http://localhost:3000/'
+const STAGE_URL = 'http://localhost:8888/api-docs'
+// const DEV_URL = 'http://3.11.202.153:8888/api'
+// const LOCAL_URL = 'http://localhost:3000/api'
 
 const APP_CONFIG = {
   rinkebyProvider: 'http://rinkeby.infura.io/v3/2de4d25aeea745b181468b898cf4e899',
@@ -7,30 +8,30 @@ const APP_CONFIG = {
   exchangeRate: (from: string, to: string) => `https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`,
 
   // GET All
-  getItemAll: `${BASE_URL}api/item/get_all`,
-  getMarketplaceAll: `${BASE_URL}api/marketplace/get_all`,
+  getItemAll: `${STAGE_URL}/item/get_all`,
+  getMarketplaceAll: `${STAGE_URL}/marketplace/get_all`,
 
   // GET User
-  getUserProfileByUserId: (id: number) => `${BASE_URL}api/user/get/${id}`,
-  getUserProfileByOwner: (owner: string) => `${BASE_URL}api/user/get/wallet/${owner}`,
-  getUserByWallet: (wallet: string) => `${BASE_URL}api/user/get/wallet/${wallet}`,
+  getUserProfileByUserId: (id: number) => `${STAGE_URL}/user/get/${id}`,
+  getUserProfileByOwner: (owner: string) => `${STAGE_URL}/user/get/wallet/${owner}`,
+  getUserByWallet: (wallet: string) => `${STAGE_URL}/user/get/wallet/${wallet}`,
 
   // GET Asset
-  getItemByTokenId: (token_id: number) => `${BASE_URL}api/item/get_by_token_id/${token_id}`,
-  getItemByItemId: (id: number) => `${BASE_URL}api/item/get/${id}`,
-  getMetadata: (id: number) => `${BASE_URL}api/metadata/get/${id}`,
-  getMarketplaceItemById: (item_id: number) => `${BASE_URL}api/marketplace/get/${item_id}`,
-  getHistory: (market_id: number) => `${BASE_URL}api/bid/get_by_market/${market_id}`,
+  getItemByTokenId: (token_id: number) => `${STAGE_URL}/item/get_by_token_id/${token_id}`,
+  getItemByItemId: (id: number) => `${STAGE_URL}/item/get/${id}`,
+  getMetadata: (id: number) => `${STAGE_URL}/metadata/get/${id}`,
+  getMarketplaceItemById: (item_id: number) => `${STAGE_URL}/marketplace/get/${item_id}`,
+  getHistory: (market_id: number) => `${STAGE_URL}/bid/get_by_market/${market_id}`,
 
   // POST
-  uploadImage: `${BASE_URL}api/image/upload`,
-  createMetadata: `${BASE_URL}api/metadata/create`,
-  createItem: `${BASE_URL}api/item/create`,
-  createSalesDetail: `${BASE_URL}api/marketplace/create`,
-  createOrder: `${BASE_URL}api/order/create`,
-  bidListItem: `${BASE_URL}api/bid/list_item`,
-  placeBid: `${BASE_URL}api/bid/place_bid`,
-  acceptBid: `${BASE_URL}api/bid/accept_bid`,
+  uploadImage: `${STAGE_URL}/image/upload`,
+  createMetadata: `${STAGE_URL}/metadata/create`,
+  createItem: `${STAGE_URL}/item/create`,
+  createSalesDetail: `${STAGE_URL}/marketplace/create`,
+  createOrder: `${STAGE_URL}/order/create`,
+  bidListItem: `${STAGE_URL}/bid/list_item`,
+  placeBid: `${STAGE_URL}/bid/place_bid`,
+  acceptBid: `${STAGE_URL}/bid/accept_bid`,
 } as const
 
 export default APP_CONFIG
