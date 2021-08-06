753b86a8a5ad178c8e1db8e0b08bcbf7be9ef893
copslikedonuts <reggisen@gmail.com>
2021-08-06 09:42:02 +0300

    fixed username reg ex

--- src/pages/AccountSettings/lib.ts
+++ src/pages/AccountSettings/lib.ts
@@ -10,7 +10,7 @@ export function useValidationSchema() {
       .string()
       .max(32)
       .required('Username is required')
-      .matches(/^[A-Za-z0-9\s]+$/, 'Use latin alphabet'),
+      .matches(/^[A-Za-z0-9\s_]+$/, 'Use latin alphabet'),
     overview: yup
       .string()
       .max(200)
