1c651716361d3a7b65d8b193aa1dd9f33cc25f36
copslikedonuts <reggisen@gmail.com>
2021-08-08 17:20:40 +0300

    DAR-485 hide artwork details buttons

--- src/pages/ArtworkDetails/components/Form/BuyNow/FormDetails.tsx
+++ src/pages/ArtworkDetails/components/Form/BuyNow/FormDetails.tsx
@@ -61,19 +61,19 @@ export default function FormBuyDetails(props: IDetailsFormProps) {
         <Box className={classes.title}>
           <Typography variant={'h2'}>{imageData?.name}</Typography>
           <Box className={classes.titleBtnCotainer}>
-            <IconButton className={classes.borderdIconButton}>
-              <ShareIcon />
-            </IconButton>
-            <IconButton
-              // Todo will be implemented in next version
-              // onClick={(event: React.SyntheticEvent<EventTarget>) => {
-              //   const target = event.currentTarget as HTMLElement
-              //   setAnchorElExtLink(target)
-              // }}
-              className={classes.borderdIconButton}
-            >
-              <ExternalLinkIcon />
-            </IconButton>
+            {/*Todo will be implemented in next version*/}
+            {/*<IconButton className={classes.borderdIconButton}>*/}
+            {/*  <ShareIcon />*/}
+            {/*</IconButton>*/}
+            {/*<IconButton*/}
+            {/*  onClick={(event: React.SyntheticEvent<EventTarget>) => {*/}
+            {/*    const target = event.currentTarget as HTMLElement*/}
+            {/*    setAnchorElExtLink(target)*/}
+            {/*  }}*/}
+            {/*  className={classes.borderdIconButton}*/}
+            {/*>*/}
+            {/*  <ExternalLinkIcon />*/}
+            {/*</IconButton>*/}
           </Box>
         </Box>
         <Box className={classes.infoRow} mb={6}>
