d1ab2c8c723251e76675c1f50d697abe88a781ad
copslikedonuts <reggisen@gmail.com>
2021-08-11 10:13:26 +0300

    DAR-420

--- src/pages/SellNFT/lib.ts
+++ src/pages/SellNFT/lib.ts
@@ -3,5 +3,6 @@ import * as yup from 'yup'
 export function useValidationSchema() {
   return yup.object().shape({
     startDate: yup.date().min(new Date(), 'Incorrect date'),
+    endDate: yup.date().min(new Date(), 'Incorrect date'),
   })
 }
