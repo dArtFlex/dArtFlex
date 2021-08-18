2b7cab324f90ad8bd5b6b79a76c128bae402928e
Dmitriy <voytenko.d@ideasoft.io>
2021-08-18 12:26:15 +0300

    [DAR-310] added - notifications listener socket

--- src/stores/sagas/index.ts
+++ src/stores/sagas/index.ts
@@ -40,7 +40,11 @@ import {
   cancelBidRequest,
 } from '../reducers/placeBid'
 import { buyNowRequest } from '../reducers/buyNow'
-import { getNotificationsRequest } from '../reducers/notifications'
+import {
+  getNotificationsRequest,
+  listenForSocketMessagesRequest,
+  updateForSocketMessagesDataRequest,
+} from '../reducers/notifications'
 import { getAllWorksRequest, getAllUsersListRequest, banUserRequest, unbanUserRequest } from '../reducers/management'
 import { makeOfferRequest } from '../reducers/makeOffer'
 
@@ -71,7 +75,7 @@ import { minting, uploadImage } from '../sagas/minting'
 import { listing, unlisting } from '../sagas/listing'
 import { placeBid, getBidsHistory, acceptBid, getBids, cancelBid } from '../sagas/placeBid'
 import { buyNow } from '../sagas/buyNow'
-import { getNotifications } from '../sagas/notifications'
+import { getNotifications, listenForSocketMessages, updateForSocketMessagesData } from '../sagas/notifications'
 import { getAllWorks, getAllUsersList, banUser, unbanUser } from '../sagas/management'
 import { makeOffer } from '../sagas/makeOffer'
 
@@ -126,6 +130,8 @@ export default function* root() {
 
     /** Notifications **/
     takeLatest(getNotificationsRequest.type, getNotifications, apiMiddleware),
+    takeLatest(listenForSocketMessagesRequest, listenForSocketMessages),
+    takeLatest(updateForSocketMessagesDataRequest, updateForSocketMessagesData),
 
     /** Management **/
     takeLatest(getAllWorksRequest.type, getAllWorks, apiMiddleware),
--- src/types/Notifications.ts
+++ src/types/Notifications.ts
@@ -1,9 +1,10 @@
-export interface INotifications {
+import { IDatedEntity, IEntity } from 'types'
+export interface INotifications extends IEntity {
   message: IMessageType
   updated_at: string
   item_id: string
   image: string
-  status: boolean
+  read: boolean
 }
 
 export type IMessageType =
@@ -11,5 +12,22 @@ export type IMessageType =
   | 'Your Item has been bid'
   | 'The price was changed'
   | 'Your auction was ended'
-  | 'Your bid  was outbid'
+  | 'Your bid was outbid'
   | 'Please be informed about significant updates to your item'
+
+export interface ISocketDataNotification extends IDatedEntity, IEntity {
+  activity_id: number
+  bid_amount: string
+  bid_id: string
+  from: string
+  item_id: string
+  market_id: string
+  order_id: string
+  read: boolean
+  sales_token_contract: string
+  status: INotificationStatus
+  to: string
+  tx_hash: null | string
+}
+
+type INotificationStatus = 'bidded'
