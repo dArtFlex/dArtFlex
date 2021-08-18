2ecff24d2175d299da00f4319c06779d16667776
aleksandr.t <aleksandr.t@ideasoft.io>
2021-08-18 10:09:12 +0300

    DAR-571

--- src/pages/Dashboard/components/Empty/index.tsx
+++ src/pages/Dashboard/components/Empty/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Box, Typography, Button } from '@material-ui/core'
 
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import { useHistory } from 'react-router-dom'
+import routes from '../../../../routes'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -40,13 +42,14 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function Empty() {
   const classes = useStyles()
+  const history = useHistory()
   return (
     <Box className={classes.container}>
       <Box className={classes.box}>
         <Typography className={classes.text}>
           Your collection of artworks is empty. Start building your collection by placing bids on artwork.
         </Typography>
-        <Button variant={'outlined'} className={classes.btn}>
+        <Button variant={'outlined'} className={classes.btn} onClick={() => history.push(routes.artworks)}>
           Go to Artworks
         </Button>
       </Box>
