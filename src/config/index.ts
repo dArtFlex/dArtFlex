96855ec8bd91ce6bdcc756074086f360bdc8819b
d.voitenko <voytenko.d@ideasoft.io>
2021-08-26 17:00:41 +0300

    [WIP] fixed - notification endpoint

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,7 +16,7 @@ const APP_CONFIG = {
   etherscanRinkeby: 'https://rinkeby.etherscan.io',
   etherscanMainnet: 'https://etherscan.io',
 
-  WSUrl: 'http://dartflex-dev.ml:8887',
+  WSUrl: 'https://dartflex-dev.ml:8887',
   baseURL: `${BASE_URL}`,
   localURL: `${LOCAL_URL}`,
 
